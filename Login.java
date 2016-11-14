b7680cbc7882d379a9ce277ddcb07b92e375a8bf
Christian Daley <christiandaley@csil.cs.ucsb.edu>
2016-11-13 16:41:19 -0800

    Added prompts

--- Login.java
+++ Login.java
@@ -1,8 +1,25 @@
 
+
+
 public class Login {
 
     public static void main(String args[]) {
 
-	System.out.println("Hello, World!");
+	System.out.println("Welcome to BuzMo\n");
+	String items[] = {"Login", "Create new account"};
+	int answer = Menu.DisplayMenu(items);
+
+	if (answer == 1) {
+	    // login
+
+	} else if (answer == 2) {
+	    // create account
+	    String prompts[] = {"Name", "Phone Number", "Email", "Password", "Screen Name"};
+	    Menu.PromptUser( prompts );
+	} else {
+	    System.out.println("Error\n");
+	}
+	
+	
     }
 }
---
+++ Menu.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int DisplayMenu( String[] items) {
+	for (int i = 0; i < items.length; i++) {
+	    System.out.println( (i + 1) + ". " + items[i] );
+	}
+
+	
+	return Integer.parseInt(scanner.nextLine());
+    }
+
+
+    public static String[] PromptUser( String[] prompts ) {
+	String answers[] = new String[prompts.length];
+
+	for (int i = 0; i < prompts.length; i++) {
+	    System.out.println( "Please enter: " + prompts[i] );
+	    answers[i] = scanner.nextLine();
+	}
+
+	return answers;
+	
+    }
+}
