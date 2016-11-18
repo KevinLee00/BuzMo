255a27db77a5a053c2b83141c9fbd6faa2186f7c
Kevin Lee <kevinlee00@umail.ucsb.edu>
2016-11-17 21:52:41 -0800

    Created User.java

---
+++ User.java
@@ -0,0 +1,10 @@
+public static class User (String[][ args) {
+	private static email; // Used to login
+
+	public static String getEmail() {
+	    return email;
+	}
+
+	public static void setEmail(String email) {
+	    User.email = email;
+}
