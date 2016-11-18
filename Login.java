c3022fe2ed9d7e74b3699d45097136578d53b842
Christian Daley <christiandaley@csil.cs.ucsb.edu>
2016-11-17 21:03:04 -0800

    Made changes to Insert in SQLHelper.java and Login.java

--- Login.java
+++ Login.java
@@ -9,13 +9,23 @@ public class Login {
 	String items[] = {"Login", "Create new account"};
 	int answer = Menu.DisplayMenu(items);
 
+
 	if (answer == 1) {
 	    // login
 
 	} else if (answer == 2) {
 	    // create account
 	    String prompts[] = {"Name", "Phone Number", "Email", "Password", "Screen Name"};
-	    SQLHelper.Insert("Users", Menu.PromptUser( prompts ));
+	    String[] answers = Menu.PromptUser( prompts );
+	    String[] temp = new String[6];
+	    temp[5] = "0";
+	    for (int i = 0; i < 5; i++) {
+		temp[i] = answers[i];
+	    }
+
+	    String types[] = {"STRING", "STRING", "STRING", "STRING", "STRING", "INTEGER"};
+	    
+	    SQLHelper.Insert("Users", temp, types);
 	} else {
 	    System.out.println("Error\n");
 	}
--- SQLHelper.java
+++ SQLHelper.java
@@ -2,27 +2,26 @@ import java.sql.*;
 
 public class SQLHelper {
     
-    public static void Insert(String table, String[] values) {
-	String columns[] = GetColumnNamesForTable(table);
-	String sql = "INSERT INTO " + table + "(";
-	String v = " VALUES (";
-	for (int i = 0; i < columns.length; i++) {
-	    String t = i == columns.length - 1 ? ")" : ", ";
-	    sql += columns[i] + t;
-	    v += "'" + values[i] + "'" + t;
+    public static void Insert(String table, String[] values, String[] types) {
+	String sql = "INSERT INTO " + table + " VALUES (";
+	for (int i = 0; i < values.length; i++) {
+	    String t = i == values.length - 1 ? ")" : ",";
+	    if (types[i] == "STRING")
+		sql += "'" + values[i] + "'";
+	    else if (types[i] == "INTEGER")
+		sql += values[i];
+
+	    sql += t;
 	}
 
-	sql += v;
-
-	SQLHelper.ExecuteSQL(sql);	
+	ExecuteSQL(sql);
     }
     
     public static void DeleteTable(String tableName) {
 	SQLHelper.ExecuteSQL("DROP TABLE " + tableName);
     }
-    
 
-    private static void ExecuteSQL(String sql) {
+    public static void ExecuteSQL(String sql) {
 	try {
 	    Class.forName("oracle.jdbc.driver.OracleDriver");
 	    String url = "jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe";
@@ -38,7 +37,7 @@ public class SQLHelper {
     
     private static String[] GetColumnNamesForTable(String table) {
 	if (table == "Users") {	
-	    	String columns[] = {"name", "phone_num", "email", "pword", "screen_name"};
+	    	String columns[] = {"name", "phone_num", "email", "pword", "screen_name, is_manager"};
 		return columns;
 	}
 
